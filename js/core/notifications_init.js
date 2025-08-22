1b78881601a27d3c97fec153d75965af25df20bb
takeourcarsnow <ignasnefas@gmail.com>
2025-08-22 23:20:55 +0300

    notif v2

---
+++ js/core/notifications_init.js
@@ -0,0 +1,6 @@
+import notificationsView from '../views/notifications_view.js';
+
+// Initialize notifications UI on DOMContentLoaded
+window.addEventListener('DOMContentLoaded', function() {
+  notificationsView.init();
+});
